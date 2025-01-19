7659a84ee02edc59f0092ce88f7b8e605fa8ac27
syasul <githubku4@gmail.com>
2025-01-19 16:43:42 +0700

    feat(FE): Styling sidebar

--- resources/views/partials/sideBarAdmin.blade.php
+++ resources/views/partials/sideBarAdmin.blade.php
@@ -8,47 +8,44 @@
         </div>
     </div>
     <ul class="sidebar-nav">
-        <li class="sidebar-item">
+        <li class="sidebar-item {{ request()->routeIs('dashboard-admin.index') ? 'active' : '' }}">
             <a href="{{ route('dashboard-admin.index') }}" class="sidebar-link">
                 <i class="fa-solid fa-gauge"></i>
                 <span>Dashboard</span>
             </a>
         </li>
-        <li class="sidebar-item">
+        <li class="sidebar-item {{ request()->routeIs('manage-user.index') ? 'active' : '' }}">
             <a href="{{ route('manage-user.index') }}" class="sidebar-link">
                 <i class="fa-regular fa-user"></i>
                 <span>Manage User</span>
             </a>
         </li>
-        <li class="sidebar-item">
+        <li class="sidebar-item {{ request()->routeIs('manage-ukm.index') ? 'active' : '' }}">
             <a href="{{ route('manage-ukm.index') }}" class="sidebar-link">
                 <i class="fa-solid fa-users-rectangle"></i>
                 <span>Manage UKM</span>
             </a>
-           
         </li>
-
-        <li class="sidebar-item">
+        <li class="sidebar-item {{ request()->routeIs('manage-laporan-ukm.index') ? 'active' : '' }}">
             <a href="{{ route('manage-laporan-ukm.index') }}" class="sidebar-link">
                 <i class="fa-regular fa-circle-check"></i>
                 <span>Laporan UKM</span>
             </a>
         </li>
-        <li class="sidebar-item">
+        <li class="sidebar-item {{ request()->routeIs('log-activity.index') ? 'active' : '' }}">
             <a href="{{ route('log-activity.index') }}" class="sidebar-link">
                 <i class="fa-regular fa-file-lines"></i>
                 <span>Log Activity</span>
             </a>
         </li>
-        
-        
     </ul>
     <div class="sidebar-footer">
         <form id="logout-form" action="{{ route('auth.logout') }}" method="POST" style="display: none;">
             @csrf
         </form>
-        
-        <a href="{{ route('auth.logout') }}" class="sidebar-link" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+
+        <a href="{{ route('auth.logout') }}" class="sidebar-link"
+            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
             <i class="fa-solid fa-arrow-right-from-bracket"></i>
             <span>Logout</span>
         </a>
--- resources/views/partials/sideBarBph.blade.php
+++ resources/views/partials/sideBarBph.blade.php
@@ -8,40 +8,38 @@
         </div>
     </div>
     <ul class="sidebar-nav">
-        <li class="sidebar-item">
+        <li class="sidebar-item {{ request()->routeIs('dashboard-ukm.index') ? 'active' : '' }}">
             <a href="{{ route('dashboard-ukm.index') }}" class="sidebar-link">
                 <i class="fa-solid fa-gauge"></i>
                 <span>Dashboard</span>
             </a>
         </li>
-        <li class="sidebar-item">
+        <li class="sidebar-item {{ request()->routeIs('manage-anggota.index') ? 'active' : '' }}">
             <a href="{{ route('manage-anggota.index') }}" class="sidebar-link">
                 <i class="fa-solid fa-user-group"></i>
                 <span>Manage Anggota</span>
             </a>
         </li>
-        <li class="sidebar-item">
+        <li class="sidebar-item {{ request()->routeIs('manage-kegiatan-ukm.index') ? 'active' : '' }}">
             <a href="{{ route('manage-kegiatan-ukm.index') }}" class="sidebar-link">
                 <i class="fa-solid fa-chart-line"></i>
                 <span>Manage Kegiatan</span>
             </a>
-            
         </li>
-        <li class="sidebar-item">
+        <li class="sidebar-item {{ request()->routeIs('manage-kas-ukm.index') ? 'active' : '' }}">
             <a href="{{ route('manage-kas-ukm.index') }}" class="sidebar-link">
                 <i class="fa-solid fa-money-bill-wave"></i>
                 <span>Manage Kas</span>
             </a>
-            
         </li>
-       
     </ul>
     <div class="sidebar-footer">
         <form id="logout-form" action="{{ route('auth.logout') }}" method="POST" style="display: none;">
             @csrf
         </form>
 
-        <a href="#" class="sidebar-link" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+        <a href="#" class="sidebar-link"
+            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
             <i class="fa-solid fa-arrow-right-from-bracket"></i>
             <span>Logout</span>
         </a>
